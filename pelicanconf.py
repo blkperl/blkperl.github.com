211776ab92933fb1a5f45cdc861b6e329b3d1035
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-03-23 14:43:27 -0700

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'William Van Hevelingen'
+SITENAME = u'zfs snapshot blkperl/dev/brain@Today'
+SITEURL = ''
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#          ('Python.org', 'http://python.org'),
+#          ('Jinja2', 'http://jinja.pocoo.org'),
+#          ,)
+
+SOCIAL = (('twitter', 'http://twitter.com/pdx_blkperl'),
+          ('github', 'http://github.com/blkperl'),)
+
+DEFAULT_PAGINATION = 10
+
+TWITTER_USERNAME='pdx_blkperl'
